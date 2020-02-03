c1ee6008d00f6206c733453e061426932c146746
Bernadette Davis <bsd231@nyu.edu>
2020-02-03 23:45:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class Triangle extends Polygon{
     if (!(Array.isArray(this.sides))) return;
     if (this.countSides !== 3) return;
     
-    if ((this.sides[0] + this.sides[1]) > this.sides[2] || (this.sides[1] + this.sides[2]) > this.sides[0] || (this.sides[2] + this.sides[0]) > this.sides[1])
+    if ((this.sides[0] + this.sides[1]) > this.sides[2] && (this.sides[1] + this.sides[2]) > this.sides[0] && (this.sides[2] + this.sides[0]) > this.sides[1])
       return false;
       
       return true;
